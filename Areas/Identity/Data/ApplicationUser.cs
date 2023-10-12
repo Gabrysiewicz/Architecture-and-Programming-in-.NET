0ca4882952d85015efc237f419f2362bcd8d6f78
Kamil Gabrysiewicz <kamilgabrysiewicz60@gmail.com>
2023-10-12 16:51:01 +0200

    Update .gitignore and untrack files

--- Areas/Identity/Data/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Laboratorium7b.Areas.Identity.Data
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public long CustomerId { get; set; }
-    }
-}
--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Laboratorium7b.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
